d7f9fc84da57ec7d401feda362fb76285feeb325
abraham2212 <ibrahimsak@code.edu.az>
2022-12-08 00:19:42 +0400

    csarp

---
+++ CSarphomework/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+//int n = 10;
+//int m = 20;
+
+//int sum = 0;
+
+//for (int i = n; i < m; i++) ;
+//{
+//    sum += m;
+//}  
+////Console.WriteLine(sum);
+
+//int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 50 };
+//for (int i = 0; i < nums.Length; i++) ;
+//{
+//    if (nums[5] == 50) ;
+//}
+//Console.WriteLine(nums[5]);
+
+//verilmis nededinin 3 e ve 7 e bolunub bolunmemesini tapin
+//int n;
+//n = 1;
+//if (n % 3 == 0 && n % 7 == 0)
+//    Console.WriteLine("n 3 e ve 7 e bolunur ");
+
+//Console.WriteLine("n 3 e ve 7 e bolunmur");
+
+//n ve m ededleri verilir eger n ve m ededleri her ikisi cutdurse n ve m ededlerinin cemin hesablayin
+
+//int n =1, m=1, sum = 0;
+//if (n % 2 == 0 && m % 2 == 0) ;
+//sum = n + m;
+//Console.WriteLine("sum");
+
+//Console.WriteLine("duzgun reqem daxil edin");
+
+
+//verilmis n ve m (n<m ededleri arasindaki tek eddlerin cemini tapin
+
+//int n, m, sum = 0;
+//n = 2;
+//m = 6;
+
+//for (int i = n; i < m; i++)
+
+//    if (i % 2 == 1 );
+//{
+//    sum += n;
+//}
+
+//Console.WriteLine(sum);
+
+
+
+//verilmis arrayin icindeki tek ededlerin cemini tapin
+
+
+
